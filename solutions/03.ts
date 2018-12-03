34a5826ee462ba72529fdd69eca1c88f184cdb5d
Kalle <kr@impera.se>
2018-12-03 14:47:42 +0100

    Optimize, refactor

--- solutions/03.ts
+++ solutions/03.ts
@@ -1,57 +1,48 @@
 import { loadData } from './utils';
 import * as _ from 'lodash';
 
-interface Point {
-	x: number;
-	y: number;
-}
 interface Square {
 	id: number;
-	position: Point;
-	size: Point;
 	uniqPoints: string[];
 }
 
-const uniqId = (x: number, y: number) => `${x}x${y}`;
-
 // #id @ X,Y: XxY
 const getData = (data: string): Square[] => {
-	const uniqPoints = (s: Square): string[] => {
-		const points: string[] = [];
-		for (let i = s.position.x; i < s.position.x + s.size.x; i++) {
-			for (let j = s.position.y; j < s.position.y + s.size.y; j++) {
-				points.push(uniqId(i, j));
-			}
-		}
-		return points;
+	const pointsToStrings = (x: number, y: number, w: number, h: number): string[] => {
+		return _.flatten(
+			_.range(x, x + w)
+				.map(currX => _.range(y, y + h).map(currY => `${currX}x${currY}`))
+		);
 	}
 
 	return data.splitLines().map(r => {
 		const matches = r.match(/#(\d+)\s+@\s+(\d+),(\d+):\s+(\d+)x(\d+)/);
-		const square = !matches ? <Square>{} : <Square>{
-			id: +matches[1],
-			position: { x: +matches[2], y: +matches[3] },
-			size: { x: +matches[4], y: +matches[5] },
-		};
-		return {...square, uniqPoints: uniqPoints(square)};
+
+		return !matches 
+			? {id: 0, uniqPoints: []} 
+			: {id: +matches[1], uniqPoints: pointsToStrings(+matches[2], +matches[3], +matches[4], +matches[5])};
 	});
 };
 
+let overlapping: string[];
 const getAllOverlapping = (data: Square[]): string[] => {
-	const foundPoints: {[id: string]: boolean} = {};
-	const uniqOverlapping: {[id: string]: boolean} = {};
-
-	data.forEach((d: Square) => {
-		const points = d.uniqPoints;
-		points.forEach(p => {
-			if (foundPoints[p]) {
-				uniqOverlapping[p] = true;
-			}
+	if (!overlapping) {
+		const foundPoints: {[id: string]: boolean} = {};
+		const uniqOverlapping: {[id: string]: boolean} = {};
+	
+		data.forEach((d: Square) => {
+			const points = d.uniqPoints;
+			points.forEach(p => {
+				if (foundPoints[p]) {
+					uniqOverlapping[p] = true;
+				}
+			});
+			points.forEach(p => foundPoints[p] = true);
 		});
-		points.forEach(p => foundPoints[p] = true);
-	});
+		overlapping = _.keys(uniqOverlapping);
+	}
 
-	return _.keys(uniqOverlapping);
+	return overlapping;
 };
 
 const part1 = (data: Square[]): number => {
