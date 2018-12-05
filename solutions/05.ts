88d316dfce4077215e1f39da5aa9feb4de053c6d
Kalle <kr@impera.se>
2018-12-05 10:16:46 +0100

    Day 5

--- solutions/05.ts
+++ solutions/05.ts
@@ -1,17 +1,29 @@
-import { loadData } from './include/utils';
+import { alphabetArray, loadData } from './include/utils';
 import * as _ from 'lodash';
 
 const day = 5;
 
-const getData = (data: string) => data;
+const transformationRex: RegExp = ((): RegExp => {
+	return new RegExp(
+		alphabetArray
+			.map(c => c.toLowerCase() + c.toUpperCase() + '|' + c.toUpperCase() + c.toLowerCase())
+			.join('|'),
+		'g'
+	);
+})();
 
-const part1 = (data: any) => data;
-const part2 = (data: any) => data;
+const transform = (data: string) => {
+	while (data.match(transformationRex)) {
+		data = data.replace(transformationRex, '');
+	}
+	return data;
+};
 
+const part1 = (data: string) => transform(data).length;
+const part2 = (data: string) => _.min(alphabetArray.map((c) => transform(data.replace(new RegExp(c, 'gi'), '')).length));
 
 // Scaffolding:
 loadData(day, (data: string) => {
-	let rows = getData(data);
-	console.log('Part1: ', part1(rows));
-	console.log('Part2: ', part2(rows));
+	console.log('Part1: ', part1(data));
+	console.log('Part2: ', part2(data));
 });
--- solutions/include/utils.ts
+++ solutions/include/utils.ts
@@ -1,6 +1,9 @@
 import * as fs from 'fs';
 import './extensions';
 
+export const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+export const alphabetArray = alphabet.splitChars();
+
 export function loadData(day: number, callback: Function): void {
 	const filename = `solutions/data/${day}.dat`;
 	fs.readFile(filename, (error, data) => {
