06adc2258355c6980773a1defdea5dcf86e0a70a
Kalle <kr@impera.se>
2018-12-06 08:37:11 +0100

    Start - day 6

--- solutions/06.ts
+++ solutions/06.ts
@@ -1,17 +1,31 @@
 import { loadData } from './include/utils';
 import * as _ from 'lodash';
 
+interface Point {
+	x: number;
+	y: number;
+}
+
 const day = 6;
 
-const getData = (data: string) => data;
+const getData = (data: string): Point[] => {
+	return data.splitLines().map(l => {
+		const [x, y] = l.split(/,\s+/).toNumbers();
+		return <Point>{x: y, y: y};
+	});
+};
 
-const part1 = (data: any) => data;
-const part2 = (data: any) => data;
+const manhattan = (a: Point, b: Point) => Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+
+const part1 = (data: Point[]) => {
+	
+};
+const part2 = (data: Point[]) => data;
 
 
 // Scaffolding:
 loadData(day, (data: string) => {
-	let rows = getData(data);
-	console.log('Part1: ', part1(rows));
-	console.log('Part2: ', part2(rows));
+	let points = getData(data);
+	console.log('Part1: ', part1(points));
+	console.log('Part2: ', part2(points));
 });
