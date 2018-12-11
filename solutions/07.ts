a51fc9ae580612a42ca8278e70701d44621b9be2
Kalle <kr@impera.se>
2018-12-11 08:35:10 +0100

    Day 7 - part 1

--- solutions/07.ts
+++ solutions/07.ts
@@ -1,17 +1,43 @@
 import { loadData } from './include/utils';
 import * as _ from 'lodash';
 
-const day = 0;
+const day = 7,
+	rex = /Step ([A-Z]) must be finished before step ([A-Z]) can begin./,
+	steps: {[name: string]: string[]} = {};
 
-const getData = (data: string) => data;
+const getData = (data: string) => {
+	data.splitLines().forEach(l => {
+		const matches = l.match(rex);
+		if (matches) {
+			const [m, dependsOn, step] = matches;
+			if (!steps[step]) {
+				steps[step] = [];
+			}
+			if (!steps[dependsOn]) {
+				steps[dependsOn] = [];
+			}
+			steps[step].push(dependsOn);
+		}
+	});
+};
 
-const part1 = (data: any) => data;
-const part2 = (data: any) => data;
+const part1 = () => {
+	let order = '';
+	const remaining = {...steps};
+	while (_.keys(remaining).length > 0) {
+		const currentStep = _.keys(remaining).filter(k => remaining[k].length === 0).sort()[0];
+		order += currentStep;
+		delete remaining[currentStep];
+		_.keys(remaining).forEach(k => remaining[k] = remaining[k].filter(d => d !== currentStep));
+	}
+	return order;
+};
+const part2 = () => {};
 
 
 // Scaffolding:
 loadData(day, (data: string) => {
-	let rows = getData(data);
-	console.log('Part1: ', part1(rows));
-	console.log('Part2: ', part2(rows));
+	getData(data);
+	console.log('Part1: ', part1());
+//	console.log('Part2: ', part2());
 });
