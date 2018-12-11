9d2f996f8d2e0af7eaacffb1e56f19277e00409b
Kalle <kr@impera.se>
2018-12-11 09:26:02 +0100

    Day 7

--- solutions/07.ts
+++ solutions/07.ts
@@ -1,6 +1,12 @@
 import { loadData } from './include/utils';
 import * as _ from 'lodash';
 
+class Step {
+	name: string;
+	dependsOn: string[];
+	timeRemaining: number;
+}
+
 const day = 7,
 	rex = /Step ([A-Z]) must be finished before step ([A-Z]) can begin./,
 	steps: {[name: string]: string[]} = {};
@@ -32,12 +38,48 @@ const part1 = () => {
 	}
 	return order;
 };
-const part2 = () => {};
+const part2 = (numWorkers = 5, minTime = 60) => {
+	let done = '',
+		seconds = 0,
+		remaining: Step[] = _.keys(steps).map(k => {
+			return <Step>{
+				name: k,
+				dependsOn: [...steps[k]],
+				timeRemaining: minTime + (k.charCodeAt(0) - 'A'.charCodeAt(0)) + 1
+			};
+		}).sort((a, b) => a.dependsOn.length - b.dependsOn.length),
+		workers: string[] = [];
+	while (remaining.length > 0) {
+		let doneThisStep = '';
+		while (workers.length < numWorkers) {
+			const currentSteps = remaining.filter(r => workers.indexOf(r.name) < 0 && r.dependsOn.length === 0);
+			if (currentSteps.length > 0) {
+				workers.push(currentSteps.map(s => s.name).sort()[0]);
+			} else {
+				break;
+			}
+		}
+		workers.map(w => remaining.find(r => r.name === w)).forEach(r => {
+			r.timeRemaining--;
+			if (r.timeRemaining === 0) {
+				doneThisStep += r.name;
+				remaining.forEach(rr => rr.dependsOn = rr.dependsOn.filter(d => d !== r.name));
+			}
+		});
+		// console.log(seconds, workers, done);
+		workers = workers.filter(w => remaining.find(r => r.name === w).timeRemaining > 0);
+		remaining = remaining.filter(r => r.timeRemaining > 0);
+		seconds++;
+		done += doneThisStep;
+	}
+	// console.log(seconds, workers, done);
+	return [done, seconds];
+};
 
 
 // Scaffolding:
 loadData(day, (data: string) => {
 	getData(data);
 	console.log('Part1: ', part1());
-//	console.log('Part2: ', part2());
+	console.log('Part2: ', part2(5, 60));
 });
