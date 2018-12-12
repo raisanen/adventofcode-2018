ccb494151e84b7bd375efebaa7e63b82d3cc6010
Kalle <kr@impera.se>
2018-12-12 09:04:01 +0100

    Progress 11, scaffold 12

--- solutions/11.ts
+++ solutions/11.ts
@@ -2,42 +2,50 @@ import { range } from './include/utils';
 import { flatten } from 'lodash';
 
 const day = 11;
+const SERIAL_NUMBER: number = 42;
 
-class Cell {
-	static SERIAL_NUMBER: number = 18;
-	x: number;
-	y: number;
+const powerLevel = (x: number, y: number): number => {
+	const rackId = x + 10;
+	let power = (
+		((rackId * y) + SERIAL_NUMBER) * rackId
+	).toString();
+	
+	return (
+		power.length < 3 
+			? 0 
+			: parseInt(power.charAt(power.length - 2))
+	) - 5;
+};
 
-	constructor(x: number, y: number) {
-		this.x = x;
-		this.y = y;
-	}
+const score = (startX: number, startY: number): number => {
+	return range(startY, startY + 3)
+		.map(y => range(startX, startX + 3)
+			.map(x => powerLevel(x, y)).sum()
+	).sum()
+};
 
-	get rackId(): number {
-		return this.x + 10;
-	}
+const part1 = () => {
+	let maxScore = 0,
+		maxCoords: {x: number, y: number} = null;
 
-	get powerLevel(): number {
-		let power = (
-			((this.rackId * this.y) + Cell.SERIAL_NUMBER) * this.rackId
-		).toString();
-		
-		return (power.length < 3 ? 0 : parseInt(power[power.length - 3])) - 5;
-	}
-}
-
-const cells: Cell[] = flatten(
-	range(0, 300)
-		.map(y => range(0, 300).map(x => new Cell(x, y)))
-);
-
-const part1 = () => {};
+	range(1, 298).forEach(y => {
+		range(1, 298).forEach(x => {
+			const thisScore = score(x, y);
+			if (thisScore > maxScore) {
+				maxScore = thisScore;
+				maxCoords = {x: x, y: y};
+			}
+			thisScore;
+		});
+	});
+	return [maxCoords, maxScore];
+};
 const part2 = () => {};
 
 // Scaffolding:
 const solve = () => {
 	console.log('Part1: ', part1());
-	console.log('Part2: ', part2());
+	console.log('Part2:', powerLevel(21,61))
 };
 
 solve();
